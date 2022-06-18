4c32168cc1a465a328fe209bc5784264ba92af22
Pandya Limawan <86063345+pan-dya@users.noreply.github.com>
2022-06-18 22:48:32 +0700

    Update Hand.java

--- blackjack/Hand.java
+++ blackjack/Hand.java
@@ -1,23 +1,24 @@
 package com.blackjack;
 
+//import
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import com.blackjack.Cards.Numbers;
 
 public class Hand {
-    private ObservableList<Node> cards;
-    private SimpleIntegerProperty value = new SimpleIntegerProperty(0);
-    private int aces = 0;
+    private ObservableList<Node> cards;         //observable list to listen to listeners
+    private SimpleIntegerProperty value = new SimpleIntegerProperty(0); //simple integer property creates a getter, setter, and allows listeners to make changes
+    private int aces = 0;   //count the aces
 
+    //constructor
     public Hand(ObservableList<Node> cards){
         this.cards = cards;
     }
 
-    public void takeCard(Cards card){
+    public void takeCard(Cards card){   //  add values of cards to the hand
         cards.add(card);
-
-        if (card.num == Numbers.ACE){
+        if (card.num == Numbers.ACE){   // ace counter
             aces++;
         }
 
@@ -30,13 +31,13 @@ public class Hand {
         }
     }
 
-    public void reset(){
+    public void reset(){    //reset hand
         cards.clear();
         value.set(0);
         aces = 0;
     }
 
-    public SimpleIntegerProperty valueProperty(){
+    public SimpleIntegerProperty valueProperty(){   //to access the value, because value is private
         return value;
     }
 }
